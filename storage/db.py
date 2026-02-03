4ca3a718c4a85373f3b55b32618ffb5ef177af84
Blu-Robin1 <omar.kaled2003@gmail.com>
2026-02-02 23:10:49 -0800

    pt1 done

--- storage/db.py
+++ storage/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-ENGINE = create_engine("sqlite:///motion&temp.db")
+ENGINE = create_engine("mysql://pyro:cynical@localhost/slop")
 
 def make_session():
     return sessionmaker(bind=ENGINE)()
